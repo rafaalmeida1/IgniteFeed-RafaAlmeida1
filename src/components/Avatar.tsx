5fdf473534ade41766a2e94d06110ebf66062de1
rafaalmeida1 <rafaempresarial312@gmail.com>
2022-07-13 15:18:27 -0300

    extend ImgHTMLAttributes on AvatarProps

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,20 +1,21 @@
+import { ImgHTMLAttributes } from 'react'
+
 import classNames from "classnames";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean;
-    src: string;
-    alt?: string;
 }
 
-export function Avatar({ hasBorder, src, alt }: AvatarProps) { //Se essa propriedade não possuir nenhum valor, o valor dela vai ser true
+export function Avatar({ hasBorder, ...props }: AvatarProps) { //Se essa propriedade não possuir nenhum valor, o valor dela vai ser true
+    //...props me traz todas as propriedades que estão na interface AvatarProps, e a interface AvatarProps, extende as propriedades de ImgHTMLAttributes
+    
     return (
         <img
             className={classNames('rounded-lg', {
                 'w-[calc(3rem+12px)] h-[calc(3rem+12px)] border-4 border-gray-800 outline outline-2 outline-green-500': hasBorder,
                 'w-[3rem] h-[3rem]': !hasBorder,
             })}
-            src={src}
-            alt={alt}
+            {...props}
         />
     )
 }
